fcf15ff7066e06bcc96d489d7d9abb3d347afe32
Thanika Natarajan <thanikanatarajan.cs22@bitsathy.ac.in>
2024-07-05 21:26:09 +0530

    first streamlit app

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello World")
+import streamlit as st
+import random
+
+# List of greeting messages
+greetings = [
+    "Hello, World!",
+    "Welcome to our app!",
+    "Greetings, user!",
+    "Nice to see you!",
+    "How's your day going?",
+    "Hello there!",
+    "Hi, thanks for visiting!",
+    "Welcome aboard!",
+    "Glad you're here!",
+    "Hey, what's up!"
+]
+
+
+def main():
+    st.title("Random Greeting Generator")
+
+    st.write("Click the button below to get a random greeting!")
+
+    if st.button("Generate Greeting"):
+        greeting = random.choice(greetings)
+        st.header(greeting)
+
+
+if __name__ == "__main__":
+    main()
